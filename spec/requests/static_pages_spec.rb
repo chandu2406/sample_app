550845d1f29bdfdf406fdb982c19d5a098fd3b68
chandramouli <sharma.cms007@gmail.com>
2012-05-31 08:24:33 +0530

    Static pages refactored

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,55 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+   
+let(:base_title)  { "Ruby on Rails Sample App"}
+
+
   describe "Home Page" do
     it "should have the h1 content Sample App" do
         visit '/static_pages/home'
-        page.should have_selector('h1', :content =>'Sample App') 
+        page.should have_selector('h1', :content =>'Home') 
    
     end
     it "should have title right" do
     visit '/static_pages/home' 
-    page.should have_selector('title', :content => "This is a Sample App| Home")
+    page.should have_selector('title', :content => "#{base_title}| Home")
   end
 end
 describe "Help Page" do
     it "should have the h1 content Sample App" do
         visit '/static_pages/help'
-        page.should have_selector('h1', :content =>'Sample App') 
+        page.should have_selector('h1', :content =>'Help') 
    
     end
     it "should have title right" do
     visit '/static_pages/help' 
-    page.should have_selector('title', :content => "This is a Sample App| Help")
+    page.should have_selector('title', :content => "#{base_title}| Help")
   end
 end
 describe "About Page" do
     it "should have the h1 content Sample App" do
         visit '/static_pages/about'
-        page.should have_selector('h1', :content =>'Sample App') 
+        page.should have_selector('h1', :content =>'About') 
    
     end
     it "should have title right" do
     visit '/static_pages/about' 
-    page.should have_selector('title', :content => "This is a Sample App| About")
+    page.should have_selector('title', :content => "#{base_title}| About")
   end
 end
+
+     
+
+       describe "Content Page" do
+         it "should have the right title" do
+           visit '/static_pages/content' 
+           page.should have_selector('title', :content => "#{base_title}| Content ")
+    end
+           it "should have h1 content" do
+           visit '/static_pages/content' 
+            page.should have_selector('h1', :content =>'Content')
+end
+ 
+   end
 end
